e15f4fef62accb64b79e8cb87372842762dc95df
Peter Hashem <peterhashem@gmail.com>
2023-09-26 20:15:01 -0400

    removed commented code and exported printPassTimes

--- index.js
+++ index.js
@@ -1,38 +1,6 @@
 const {
-  fetchMyIP,
-  fetchCoordsByIP,
-  fetchISSFlyOverTimes,
   nextISSTimesForMyLocation,
 } = require("./iss");
-
-/* fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
-
-  console.log("It worked! Returned IP:", ip);
-}); */
-
-/* fetchCoordsByIP("99.251.180.120", (error, coordinates) => {
-  if (error) {
-    console.log("Error fetching coordinates:", error);
-    return;
-  }
-
-  console.log("It worked! Returned coordinates:", coordinates);
-}); */
-
-/* const exampleCoords = { latitude: "49.27670", longitude: "-123.13000" };
-
-fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
-  if (error) {
-    console.log("Error fetching duration:", error);
-    return;
-  }
-
-  console.log("It worked! Returned Flyover times:", passTimes);
-}); */
 
 const printPassTimes = function (passTimes) {
   for (const pass of passTimes) {
@@ -50,3 +18,5 @@ nextISSTimesForMyLocation((error, passTimes) => {
   // success, print out the deets!
   printPassTimes(passTimes);
 });
+
+module.exports = { printPassTimes };
